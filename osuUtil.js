283e125e38b017765978b63f2a7b1fa0f189d983
HorizonCode <horizoncode88@gmail.com>
2022-10-16 16:47:18 +0200

    detect valid osu folder by matches

---
+++ osuUtil.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+const osuEntities = [
+    'avcodec-51.dll',
+    'avformat-52.dll',
+    'avutil-49.dll',
+    'bass.dll',
+    'bass_fx.dll',
+    'collection.db',
+    'd3dcompiler_47.dll',
+    'Data',
+    'Downloads',
+    'libEGL.dll',
+    'libGLESv2.dll',
+    'Logs',
+    'Microsoft.Ink.dll',
+    'OpenTK.dll',
+    'osu!.cfg',
+    'osu!.db',
+    'osu!.exe',
+    'osu!auth.dll',
+    'osu!gameplay.dll',
+    'osu!seasonal.dll',
+    'osu!ui.dll',
+    'presence.db',
+    'pthreadGC2.dll',
+    'scores.db',
+    'Skins',
+    'Songs'
+]
+
+async function isValidOsuFolder(path) {
+    const allFiles = await fs.promises.readdir(path);
+    let matches = 0;
+    for (const file of allFiles) {
+        if (osuEntities.includes(file)) matches = matches + 1;
+    }
+    return Math.round((matches / osuEntities.length) * 100) >= 60;
+}
+
+module.exports = { isValidOsuFolder }
