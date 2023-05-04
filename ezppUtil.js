48cd81f28d9ab29d5f1ed6f5fc06c17a51e24692
HorizonCode <horizoncode88@gmail.com>
2023-05-04 21:22:22 +0200

    update url

--- ezppUtil.js
+++ ezppUtil.js
@@ -1,6 +1,6 @@
 const axios = require('axios').default;
 
-const loginCheckEndpoint = 'https://new.ez-pp.farm/login/check';
+const loginCheckEndpoint = 'https://ez-pp.farm/login/check';
 
 const performLogin = async (username, password) => {
     const result = await axios.post(loginCheckEndpoint, { username, password });
