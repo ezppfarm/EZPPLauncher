5b1407638ae4c77ce5a45fae88facc2e9a9c1713
HorizonCode <horizoncode88@gmail.com>
2024-01-25 14:10:18 +0100

    add dialog for unhandled errors

--- main.js
+++ main.js
@@ -2,6 +2,16 @@
 const { app, BrowserWindow, Menu, ipcMain, dialog, shell } = require(
   "electron",
 );
+
+const unhandled = require("electron-unhandled");
+unhandled({
+  logger: console.error,
+  showDialog: true,
+  reportButton: () => {
+    shell.openExternal("https://ez-pp.farm/discord");
+  },
+});
+
 const path = require("path");
 const serve = require("electron-serve");
 const loadURL = serve({ directory: "public" });
