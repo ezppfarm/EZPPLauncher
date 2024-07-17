74e233ecc3d0216e99fe7d22de4417cc1e6b5c37
HorizonCode <horizoncode88@gmail.com>
2024-07-17 10:24:13 +0200

    add useragent headers

--- main.js
+++ main.js
@@ -339,6 +339,8 @@ function registerIPCPipes() {
         }),
         headers: {
           "Content-Type": "application/json",
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0",
         },
       });
 
