428ead85256df8f73a9277cd1039dc6ddd0111c8
HorizonCode <horizoncode88@gmail.com>
2025-07-02 11:44:31 +0200

    feat: add tauri-plugin-cors-fetch and update configuration for enhanced security

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -137,7 +137,7 @@ fn find_osu_installation() -> Option<String> {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let mut builder = tauri::Builder::default().plugin(tauri_plugin_fs::init());
+    let mut builder = tauri::Builder::default();
     #[cfg(desktop)]
     {
         builder = builder.plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
@@ -155,8 +155,10 @@ pub fn run() {
             find_osu_installation,
             valid_osu_folder
         ])
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_cors_fetch::init())
         .plugin(tauri_plugin_sql::Builder::default().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
