4e4998671afe9855e88bbc57517be1555ee526e1
HorizonCode <horizoncode88@gmail.com>
2025-07-16 16:17:41 +0200

    chore: add todo text

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -308,6 +308,7 @@ pub async fn is_net8_installed() -> bool {
     }
 }
 
+//TODO: maybe switch to this crate: https://crates.io/crates/windows-dpapi
 
 #[cfg(windows)]
 pub fn encrypt_password(password: &str) -> Result<String, String> {
