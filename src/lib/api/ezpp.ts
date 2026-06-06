992127ecb6bc80dcb4831f7d502e6b4026d14bdc
HorizonCode <horizoncode88@gmail.com>
2026-06-07 00:13:55 +0200

    fix: only accept json

--- src/lib/api/ezpp.ts
+++ src/lib/api/ezpp.ts
@@ -98,6 +98,7 @@ export const ezppfarm = {
         stream: 'none',
       },
       headers: {
+        Accept: 'application/json',
         'Content-Type': 'application/json',
         'User-Agent': 'EZPPLauncher',
       },
